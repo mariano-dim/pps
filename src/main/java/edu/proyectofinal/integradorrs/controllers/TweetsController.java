d45f6735dac6678b7dfbc0a25680ad4cf4430dcd
damian85 <damian.rivas@gmail.com>
2016-11-06 20:17:44 -0200

    Se agregaron:
    - Eliminación de Token en la BD
    - Filtro Cross Origin (CORS) general para todo tipo de operaciones HTTP
    - Clases de Julian, con seguridad Web comentada

---
+++ src/main/java/edu/proyectofinal/integradorrs/controllers/TweetsController.java
@@ -0,0 +1,53 @@
+package edu.proyectofinal.integradorrs.controllers;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.proyectofinal.integradorrs.model.TweetsModel;
+import edu.proyectofinal.integradorrs.services.tweets.TweetsService;
+import java.util.Collection;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import twitter4j.Status;
+
+/**
+ *
+ * @author MarianoAndres
+ */
+@RestController
+@CrossOrigin
+@RequestMapping("/api/tweets")
+public class TweetsController extends AbstractController<Status> {
+
+    @Autowired
+    private TweetsService tweetsService;
+    
+
+    @RequestMapping(method = RequestMethod.GET, value="/email/{email:.+}")
+    public ResponseEntity<Collection<Status>> getTimeline(@Validated @PathVariable("email") String email) {
+
+        System.out.println("getAll");
+
+        Collection<Status> Status = tweetsService.getAllTweets(email);
+
+        return super.collectionResult(Status);
+
+    }
+    
+    @RequestMapping(method = RequestMethod.GET, value="/UserTimeline/{user:.+}")
+    public ResponseEntity<Collection<Status>> getUserTimeline(@Validated @PathVariable("user") String user, String email) {
+
+        System.out.println("getAll");
+
+        Collection<Status> Status = tweetsService.getUserTimeline(user, email);
+
+        return super.collectionResult(Status);
+
+    }
+
+}
