d007375ef0765c93c2038058aa365e53c774289c
damian85 <damian.rivas@gmail.com>
2016-11-17 10:22:35 -0300

    - Fix en funcion de consulta de tokens (No se habian subido los java de servicio usuario)

--- src/main/java/edu/proyectofinal/integradorrs/services/usuario/UsuarioService.java
+++ src/main/java/edu/proyectofinal/integradorrs/services/usuario/UsuarioService.java
@@ -1,17 +1,25 @@
 package edu.proyectofinal.integradorrs.services.usuario;
 
+import edu.proyectofinal.integradorrs.model.Token;
 import java.util.Collection;
 
-import edu.proyectofinal.integradorrs.model.Token;
 import edu.proyectofinal.integradorrs.model.Usuario;
 
 public interface UsuarioService {
 
 	Collection<Usuario> getAllUsuarios();
+	
 	Usuario getByEmail(String email);
-    Collection<Token> getTokenByEmail(String email);
+        
+        //Collection<Token> getTokenByEmail(String email);
+        Token getTokenByEmail(String email);
+	
 	Usuario getById(String id);
+
 	Usuario save(Usuario t);
-    Token saveToken(Token t);
-    boolean saveUser(Usuario user);
+        
+        Token saveToken(Token t);
+        
+       // Token deleteToken(Token t);
+        Token deleteToken(String email);
 }
