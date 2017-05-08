ba7266226bb77be4c54a8620a9f02b5ee67ec798
Mariano Di Maggio <mariano.dim@gmail.com>
2017-05-08 17:34:07 -0300

    ingreso manejo de emails

--- src/main/java/edu/proyectofinal/integradorrs/Application.java
+++ src/main/java/edu/proyectofinal/integradorrs/Application.java
@@ -1,16 +1,23 @@
 package edu.proyectofinal.integradorrs;
 
+import org.springframework.beans.BeansException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-@RestController
-public class Application {
+import it.ozimov.springboot.mail.configuration.EnableEmailTools;
 
+@SpringBootApplication
+@EnableEmailTools
+@RestController
+public class Application implements ApplicationContextAware{
+	
+	private ApplicationContext applicationContext;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
@@ -18,5 +25,10 @@ public class Application {
 	@RequestMapping("/echo")
 	public String echo(@RequestParam String comm) {
 		return "ping";
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;		
 	}
 }
