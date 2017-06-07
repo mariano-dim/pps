339e1e5b5c61eaef7f11921f84667d963541267b
Julian.Mojico <jmojico@gmail.com>
2017-06-07 16:59:33 -0300

    merge con master

--- src/main/java/edu/proyectofinal/integradorrs/Application.java
+++ src/main/java/edu/proyectofinal/integradorrs/Application.java
@@ -1,5 +1,7 @@
 package edu.proyectofinal.integradorrs;
 
+
+
 import org.springframework.beans.BeansException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
