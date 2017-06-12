3aa4f8ed58b411d15c8b89a458fd37166a89df2c
ecorsaro <ecorsaro@W7-Dev-001.lab.local>
2017-06-12 09:41:54 -0300

    Bug fix

--- src/main/java/edu/proyectofinal/integradorrs/configurations/WebSecurityConfig.java
+++ src/main/java/edu/proyectofinal/integradorrs/configurations/WebSecurityConfig.java
@@ -1,25 +1,46 @@
 package edu.proyectofinal.integradorrs.configurations;
 
+import org.hibernate.validator.internal.metadata.raw.ConfigurationSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        
+         /*final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+         final CorsConfiguration config = new CorsConfiguration();
+
+        config.addAllowedOrigin("http://localhost:5000/");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        source.registerCorsConfiguration("/**", config);*/
+        
         http
+            /*.cors().configurationSource(source)
+                .and()*/
             .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
-                .anyRequest().authenticated()
+                //.antMatchers("http://localhost:8080/**").permitAll()
+                .anyRequest().permitAll()
+                //.anyRequest().authenticated()
                 .and()
-            .formLogin()
+            /*.formLogin()
                 .loginPage("/login")
                 .permitAll()
+                .and()*/
+            .headers()
                 .and()
             .logout()
                 .permitAll();
@@ -31,4 +52,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .inMemoryAuthentication()
                 .withUser("user").password("password").roles("USER");
     }
+   
 }
